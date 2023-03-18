ba5cd9bc4aa795105cd647ea59b1c892ff1d8d41
BradleyTan <112063334+BradleyTan@users.noreply.github.com>
2023-03-18 23:43:30 +0800

    Initial commit

---
+++ Bradley/footer.php
@@ -0,0 +1,9 @@
+//original version
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2018 Brought to You By <a href="https://freeprojectscodes.com/">freeprojectscodes</a></strong>
+    </div>
+</footer>
