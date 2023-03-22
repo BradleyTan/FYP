cad786f8c5358849575eda1ff3ddbbe59a86f6bc
BradleyTan <112063334+BradleyTan@users.noreply.github.com>
2023-03-22 11:18:42 +0800

    Update login.php

--- ecommerce/login.php
+++ ecommerce/login.php
@@ -5,6 +5,25 @@
   }
 ?>
 <?php include 'includes/header.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+  <title>Document</title>
+  <style>
+    #togglePassword {
+    position: absolute;
+    right: 618px;
+    top: 220px;
+    cursor: pointer;
+}
+  </style>
+</head>
+
 <body class="hold-transition login-page">
 <div class="login-box">
   	<?php
@@ -33,9 +52,9 @@
         		<input type="email" class="form-control" name="email" placeholder="Email" required>
         		<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       		</div>
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control" name="password" placeholder="Password" required>
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          <div class="form-group has-feedback2">
+            <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+            <span class="bi bi-eye-slash" id="togglePassword"></span>
           </div>
       		<div class="row">
     			<div class="col-xs-4">
@@ -43,6 +62,7 @@
         		</div>
       		</div>
     	</form>
+
       <br>
       <a href="password_forgot.php">I forgot my password</a><br>
       <a href="signup.php" class="text-center">Register a new membership</a><br>
@@ -51,5 +71,22 @@
 </div>
 	
 <?php include 'includes/scripts.php' ?>
+
+<script>
+        const togglePassword = document.querySelector("#togglePassword");
+        const password = document.querySelector("#password");
+
+        togglePassword.addEventListener("click", function () {
+            // toggle the type attribute
+            const type = password.getAttribute("type") === "password" ? "text" : "password";
+            password.setAttribute("type", type);
+            
+            // toggle the icon
+            this.classList.toggle("bi-eye");
+        });
+
+    </script>
+
 </body>
 </html>
+
