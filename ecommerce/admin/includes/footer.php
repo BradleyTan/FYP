ade2603f31643af85f0970b831d565c9ad97427d
BradleyTan <112063334+BradleyTan@users.noreply.github.com>
2023-04-02 16:14:19 +0800

    Initial commit

--- ecommerce/admin/includes/footer.php
+++ ecommerce/admin/includes/footer.php
@@ -1,6 +1,101 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>All rights reserved</b>
-    </div>
-    <strong>Copyright &copy; 2022 Brought To You By<a href="https://freeprojectscodes.com/">freeprojectscodes</a></strong>
-</footer>
+<style>
+ #color{
+    color: white;
+  }
+  </style>
+<footer id="footer" class="footer color-bg">
+	  <div class="links-social inner-top-sm">
+        <div class="container" style="margin-right: 50px">
+            <div class="row">
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT INFO ============================================================= -->
+<div class="contact-info" >
+    <div class="footer-logo">
+        <div class="logo">
+            <a href="home.php">
+                
+<h3>About us</h3>
+            </a>
+        </div><!-- /.logo -->
+    
+    </div><!-- /.footer-logo -->
+
+     <div class="module-body m-t-20">
+        <p class="about-us" id="color">ShopEz is an online grocery store that offers a wide selection of high-quality products with excellent customer service. They prioritize convenience and make grocery shopping easy and efficient.</p>
+    
+    </div><!-- /.module-body -->
+
+</div><!-- /.contact-info -->
+<!-- ============================================================= CONTACT INFO : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT TIMING============================================================= -->
+<div class="contact-timing">
+	<div class="module-heading">
+    <a href="home.php">
+		<h3 class="module-title">Business hour</h3>
+        </a>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+		<div class="table-responsive">
+			<table class="table">
+				<tbody>
+					<tr><td id="color">Monday-Friday:</td><td class="pull-right" id="color">9.00 To 18.00(GMT+8)</td></tr>
+					<tr><td id="color">Saturday:</td><td class="pull-right" id="color">9.00 To 12.00(GMT+8)</td></tr>
+				</tbody>
+			</table>
+		</div><!-- /.table-responsive -->
+	</div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= CONTACT TIMING : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= INFORMATION============================================================= -->
+<div class="contact-information">
+	<div class="module-heading">
+    <a href="home.php">
+		<h3 class="module-title">Contact Us</h3>
+        </a>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+        <ul class="toggle-footer" style="">
+            <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p id="color">Melaka Tengah, Malaysia</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p id="color">+60 12-345 6789<br>+60 12-834 4545</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <span><p id="color">shopez@shopping.com</p></span>
+                </div>
+            </li>
+              
+
+    
