a3d54d4be448e66b1e905443f0884241a8926266
BradleyTan <112063334+BradleyTan@users.noreply.github.com>
2023-04-02 18:28:42 +0800

    Initial commit

--- ecommerce/admin/category.php
+++ ecommerce/admin/category.php
@@ -69,7 +69,6 @@
                             <td>".$row['name']."</td>
                             <td>
                               <button class='btn btn-success btn-sm edit btn-flat' data-id='".$row['id']."'><i class='fa fa-edit'></i> Edit</button>
-                              <button class='btn btn-danger btn-sm delete btn-flat' data-id='".$row['id']."'><i class='fa fa-trash'></i> Delete</button>
                             </td>
                           </tr>
                         ";
