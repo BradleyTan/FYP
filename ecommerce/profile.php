92f133f6d5d483924346560e6a54762908773583
BradleyTan <112063334+BradleyTan@users.noreply.github.com>
2023-04-25 09:57:16 +0800

    Initial commit

--- ecommerce/profile.php
+++ ecommerce/profile.php
@@ -69,6 +69,17 @@
 	        			<div class="box-header with-border">
 	        				<h4 class="box-title"><i class="fa fa-calendar"></i> <b>Transaction History</b></h4>
 	        			</div>
+						<div class="pull-right">
+							<form method="POST" class="form-inline" action="admin/sales_print.php">
+							<div class="input-group">
+								<div class="input-group-addon">
+								<i class="fa fa-calendar"></i>
+								</div>
+								<input type="text" class="form-control pull-right col-sm-8" id="reservation" name="date_range">
+							</div>
+							<button type="submit" class="btn btn-success btn-sm btn-flat" name="print"><span class="glyphicon glyphicon-print"></span> Print</button>
+							</form>
+						</div>
 	        			<div class="box-body">
 	        				<table class="table table-bordered" id="example1">
 	        					<thead>
@@ -130,6 +141,49 @@
 </div>
 
 <?php include 'includes/scripts.php'; ?>
+<!-- Date Picker -->
+<script>
+$(function(){
+  //Date picker
+  $('#datepicker_add').datepicker({
+    autoclose: true,
+    format: 'yyyy-mm-dd'
+  })
+  $('#datepicker_edit').datepicker({
+    autoclose: true,
+    format: 'yyyy-mm-dd'
+  })
+
+  //Timepicker
+  $('.timepicker').timepicker({
+    showInputs: false
+  })
+
+  //Date range picker
+  $('#reservation').daterangepicker()
+  //Date range picker with time picker
+  $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A' })
+  //Date range as a button
+  $('#daterange-btn').daterangepicker(
+    {
+      ranges   : {
+        'Today'       : [moment(), moment()],
+        'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+        'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+        'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      },
+      startDate: moment().subtract(29, 'days'),
+      endDate  : moment()
+    },
+    function (start, end) {
+      $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+    }
+  )
+  
+});
+</script>
 <script>
 $(function(){
 	$(document).on('click', '.transact', function(e){
