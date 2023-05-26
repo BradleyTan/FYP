1063383af05e436def7bebac947010dacc1d855f
BradleyTan <112063334+BradleyTan@users.noreply.github.com>
2023-05-27 02:33:42 +0800

    Update navbar.php

--- ecommerce/admin/includes/navbar.php
+++ ecommerce/admin/includes/navbar.php
@@ -2,7 +2,7 @@
   <!-- Logo -->
   <a href="home.php" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
-    <span class="logo-mini"><b>C</b>P</span>
+    <span class="logo-mini"><b>S</b>E</span>
     <!-- logo for regular state and mobile devices -->
     <span class="logo-lg"><b>Shop</b>Ez</span>
   </a>
