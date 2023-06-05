c968fa54b05faee8734703e722297a4200b9c6de
BradleyTan <112063334+BradleyTan@users.noreply.github.com>
2023-06-06 02:08:06 +0800

    Initial commit

--- ecommerce/admin/updateOrderStatus.php
+++ ecommerce/admin/updateOrderStatus.php
@@ -8,9 +8,14 @@ if (isset($_POST['edit'])) {
     $conn = $pdo->open();
 
     try {
-        $stmt = $conn->prepare("UPDATE orders SET order_status=:order_status WHERE order_id=:order_id");
-        $stmt->execute(['order_status' => $status, 'order_id' => $id]);
+        $stmt1 = $conn->prepare("UPDATE orders SET order_status=:order_status WHERE order_id=:order_id");
+        $stmt1->execute(['order_status' => $status, 'order_id' => $id]);
+
+        $stmt2 = $conn->prepare("UPDATE sales SET orderStatus = :order_status WHERE id = :order_id");
+        $stmt2->execute(['order_status' => $status, 'order_id' => $id]);
+        
         $_SESSION['success'] = 'Order status updated successfully';
+
     } catch (PDOException $e) {
         $_SESSION['error'] = $e->getMessage();
     }
@@ -22,4 +27,3 @@ if (isset($_POST['edit'])) {
 
 header('location: orders.php');
 ?>
-
