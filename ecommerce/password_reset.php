943abfacddb33bb85decab05046031b3f708fb17
BradleyTan <112063334+BradleyTan@users.noreply.github.com>
2023-06-06 18:19:18 +0800

    Update password_reset.php

--- ecommerce/password_reset.php
+++ ecommerce/password_reset.php
@@ -34,11 +34,11 @@
 
     	<form action="password_new.php?code=<?php echo $_GET['code']; ?>&user=<?php echo $_GET['user']; ?>" method="POST">
       		<div class="form-group has-feedback">
-        		<input type="password" class="form-control" name="password" placeholder="New password" <input type="password" class="form-control" id="pass" name="password" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
+        		<input type="password" class="form-control" name="password" placeholder="New password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
         		<span class="glyphicon glyphicon-lock form-control-feedback"></span>
       		</div>
           <div class="form-group has-feedback">
-            <input type="password" class="form-control" name="repassword" placeholder="Re-type password" <input type="password" class="form-control" id="pass" name="password" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
+            <input type="password" class="form-control" name="repassword" placeholder="Re-type password" title="Retype password must be the same as the password above" required>
             <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
           </div>
       		<div class="row">
