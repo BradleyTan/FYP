9027d5fffcf9d22f0e94e11ccd9fd72fb40d1b62
BradleyTan <112063334+BradleyTan@users.noreply.github.com>
2023-06-26 18:00:44 +0800

    Update orders_row.php

--- ecommerce/admin/orders_row.php
+++ ecommerce/admin/orders_row.php
@@ -6,14 +6,15 @@ if(isset($_POST['order_id'])){
     
     $conn = $pdo->open();
 
-    $stmt = $conn->prepare("SELECT * FROM orders WHERE order_id=:order_id");
+    $stmt = $conn->prepare("SELECT sales.id AS order_id, CONCAT(users.firstname, ' ', users.lastname) AS user_name, sales.sales_date AS order_date, sales.orderStatus AS order_status
+                            FROM sales
+                            INNER JOIN users ON sales.user_id = users.id
+                            WHERE sales.id=:order_id");
     $stmt->execute(['order_id'=>$id]);
     $row = $stmt->fetch();
     
     $pdo->close();
 
-
     echo json_encode($row);
 }
 ?>
-
