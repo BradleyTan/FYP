8ac6513e8a8754bb98dd2bc6369ad062e14cdea8
BradleyTan <112063334+BradleyTan@users.noreply.github.com>
2023-06-26 23:39:04 +0800

    Update index.php

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -8,8 +8,7 @@
 	<?php include 'includes/navbar.php'; ?>
 	 
 	  <div class="content-wrapper">
-	    <div class="container">
-
+	    <div class="container">	
 	      <!-- Main content -->
 	      <section class="content">
 	        <div class="row">
